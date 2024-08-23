3c9f2bb445b855c6a11bee8ed95c5ebfb2e0c9aa
omrozh <53612475+omrozh@users.noreply.github.com>
2024-08-23 16:41:30 +0300

    h

--- app.py
+++ app.py
@@ -233,10 +233,7 @@ def profile():
 @app.route("/cancel/<doc_uuid>")
 def cancel_doc(doc_uuid):
     doc = Document.query.filter_by(document_uuid=doc_uuid).first()
-    print(doc.owner)
-    print(current_user.id)
-    print(doc.signature.status)
-    if not doc.owner == current_user.id:
+    if not doc.owner.id == current_user.id:
         return flask.redirect("/dashboard")
     if doc.signature.status == "İmzalandı":
         return flask.redirect("/dashboard")
